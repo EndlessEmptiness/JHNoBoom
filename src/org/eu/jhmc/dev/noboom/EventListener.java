2984021c93e3d3d4503db04032477bbb04a30575
JH-Organization <107820172+JH-Organization@users.noreply.github.com>
2022-07-23 09:57:54 +0800

    Add files via upload

--- src/org/eu/jhmc/dev/noboom/EventListener.java
+++ src/org/eu/jhmc/dev/noboom/EventListener.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static org.eu.jhmc.dev.noboom.plugin.mcplugin;
+
 public class EventListener implements Listener {
     private static boolean noboom_enable = true;
 
@@ -20,10 +22,19 @@ public class EventListener implements Listener {
     private static boolean allow_crepper = false;
     private static boolean allow_tntMinecart = false;
     public static void init_cfg() {
-        noboom_enable = plugin.config.getBoolean("jhmcConfig.noboom.enable_noboom",true);
-        allow_tnt = plugin.config.getBoolean("jhmcConfig.noboom.allow_tnt",false);
-        allow_crepper = plugin.config.getBoolean("jhmcConfig.noboom.allow_crepper",false);
-        allow_tntMinecart = plugin.config.getBoolean("jhmcConfig.noboom.allow_tnt_minecart",false);
+        noboom_enable = mcplugin.getConfig().getBoolean("jhmcConfig.noboom.enable_noboom",true);
+        allow_tnt = mcplugin.getConfig().getBoolean("jhmcConfig.noboom.allow_tnt",false);
+        allow_crepper = mcplugin.getConfig().getBoolean("jhmcConfig.noboom.allow_crepper",false);
+        allow_tntMinecart = mcplugin.getConfig().getBoolean("jhmcConfig.noboom.allow_tnt_minecart",false);
+        if(allow_tnt) {
+            plugin.logger.info("Allow tnt");
+        }
+        if(allow_crepper) {
+            plugin.logger.info("Allow Crepper");
+        }
+        if(allow_tntMinecart) {
+            plugin.logger.info("Allow tntMinecart");
+        }
     }
 
     @EventHandler
